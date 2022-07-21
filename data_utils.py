a21584fd019226a9f045e17abdd5f87f8656ad70
Dasaem Jeong <dasaemj@sogang.ac.kr>
2022-07-21 12:16:27 +0900

    add pre-process

---
+++ data_utils.py
@@ -0,0 +1,84 @@
+from unicodedata import category
+import torch 
+import torchaudio
+from pathlib import Path
+from tqdm.auto import tqdm
+
+
+class NeutuneSet:
+  def __init__(self, path, sr=32000) -> None:
+    self.path = Path(path)
+    self.wav_list = sorted(list(self.path.rglob('*.wav')))
+    self.sr = sr
+
+  def __len__(self):
+    return len(self.wav_list)
+  
+  def __getitem__(self, idx):
+    wav_path = self.wav_list[idx]
+    audio, sr = torchaudio.load(wav_path)
+    audio = audio.mean(0)
+    if sr != self.sr:
+      audio = torchaudio.functional.resample(audio, sr, self.sr)
+    return audio
+
+class OnFlyAudio(NeutuneSet):
+  def __init__(self, path, sr=32000) -> None:
+    super().__init__(path, sr)
+
+  def __getitem__(self, idx):
+    wav_path = self.wav_list[idx]
+    audio, sr = torchaudio.load(wav_path)
+    audio = audio.mean(0)
+    if sr != self.sr:
+      audio = torchaudio.functional.resample(audio, sr, self.sr)
+    return audio
+
+class PreProcAudio(NeutuneSet):
+  def __init__(self, path, sr=32000, pre_process=False) -> None:
+    super().__init__(path, sr)
+    if pre_process:
+      self.check_and_save_pt()
+  
+  def check_and_save_pt(self, skip_exist=True, vocab=None):
+    '''
+    Arguments:
+      skip_exist (bool): Skip pre-processing for file which have a corersponding pt file
+      vocab (list, optional): index-to-category_name 
+    '''
+    for wav_path in tqdm(self.wav_list):
+      pt_path = wav_path.with_suffix('.pt')
+      if skip_exist and pt_path.exists() :
+        continue
+      audio, sr = torchaudio.load(wav_path)
+      audio = audio.mean(0)
+      if sr != self.sr:
+        audio = torchaudio.functional.resample(audio, sr, self.sr)
+      category_index = int(pt_path.stem.split('-')[2]) - 1
+      data_dict = {'audio': audio, 'label':category_index}
+      if isinstance(vocab, list):
+        data_dict['label-str']: vocab[category_index]
+      torch.save(data_dict, pt_path)
+
+  
+  def __getitem__(self, idx):
+    '''
+    
+    Output:
+      audio_sample (torch.Tensor): 1D tensor
+      category_label (int)
+    '''
+    pt_path = self.wav_list[idx].with_suffix('.pt')
+    datasample = torch.load(pt_path)
+
+    return datasample['audio'], datasample['label']
+
+def pad_collate(raw_batch):
+  lens = [len(x) for x in raw_batch]
+  max_len = max(lens)
+  output = torch.zeros(len(raw_batch), max_len)
+
+  for i, sample in enumerate(raw_batch):
+    output[i, :len(sample)] = sample
+  
+  return output, lens
---
+++ pre_process.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import argparse
+import json
+from data_utils import PreProcAudio
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser("sed_inference")
+  parser.add_argument('--path', type=str, default='/home/teo/userdata/nia_dataset',
+                      help='Directory path of the dataset')
+  parser.add_argument('--vocab_path', type=str, default='vocab.json',
+                      help='json path for the vocabulary')
+  parser.add_argument('--sr', type=int, default=32000,
+                      help='Target sampling rate')
+
+  args = parser.parse_args()
+
+  # with open(args.vocab_path, 'r') as f:
+  #   vocab = json.load(f)
+
+  dataset = PreProcAudio(args.path, args.sr, pre_process=True)
+
+  sample_out = dataset[0]
+  dataset[0]
