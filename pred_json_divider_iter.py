b148ad5e9473c62a35b844269bcae9e0dae21f91
clay <clayryu328@gmail.com>
2022-08-30 20:36:25 +0900

    data_v4_02_import_ready

--- pred_json_divider_iter.py
+++ pred_json_divider_iter.py
@@ -1,6 +1,7 @@
 import json
 from pathlib import Path
 import argparse
+import random
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser("pred_json_divider")
@@ -10,6 +11,8 @@ if __name__ == "__main__":
                       help='hop size between two consecutive data')
   parser.add_argument('--iter', type=int, default=40,
                       help='iter number for divide')
+  parser.add_argument('--sampling', type=int, default=0,
+                      help='choose to sample the data 0 is False, 1 is True')
   
   args = parser.parse_args()
   
@@ -17,8 +20,11 @@ if __name__ == "__main__":
   with open(prediction_path, 'r') as f:
     pred_json = json.load(f)
   
+  if args.sampling:
+    random.shuffle(pred_json)
+  
   for i in range(args.iter):
     divided_pred_json = pred_json[args.hop_size*i:args.hop_size*(i+1)]
   
-    with open(f"kor_data_v4_02_1200/{args.hop_size*i}to{args.hop_size*(i+1)}.json", "w") as json_file:
+    with open(f"kor_data_v4_02_1200_sampled/{args.hop_size*i}to{args.hop_size*(i+1)}.json", "w") as json_file:
       json.dump(divided_pred_json, json_file, ensure_ascii=False)
