69e3e03aff6b04c8bd08d5359fbf1ce7c21b1195
Dasaem Jeong <dasaemj@sogang.ac.kr>
2022-09-01 21:48:34 +0900

    old jsons

---
+++ create_meata_with_ann.py
@@ -0,0 +1,14 @@
+from meta_utils import MetaCreator
+import argparse
+
+
+def get_argument_parser():
+  parser = argparse.ArgumentParser(description='Create metadata file with annotations')
+
+  
+  return
+
+
+if __name__ == "__main__":
+
+  converter = MetaCreator()
